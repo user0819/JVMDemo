31c4a9a1727ccbd0231f598429bfd4980613038d
wangxiang <81965848@qq.com>
2021-11-04 23:46:32 +0800

    Initial commit

---
+++ chapter02/src/com/atguigu/java/ClassInitTest.java
@@ -0,0 +1,24 @@
+package com.atguigu.java;
+
+/**
+ * @author shkstart
+ * @create 2020 下午 6:01
+ */
+public class ClassInitTest {
+   private static int num = 1;
+
+   static{
+       num = 2;
+       number = 20;
+       System.out.println(num);
+       //System.out.println(number);//报错：非法的前向引用。
+   }
+
+   private static int number = 10;  //linking之prepare: number = 0 --> initial: 20 --> 10
+
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println(ClassInitTest.num);//2
+        System.out.println(ClassInitTest.number);//10
+        Thread.sleep(6000);
+    }
+}
